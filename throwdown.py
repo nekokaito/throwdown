0f144b9ae1ed709496c7639cfa77b937b258127a
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2025-08-27 22:22:50 +0600

    fixed sone bugss

--- throwdown.py
+++ throwdown.py
@@ -5,42 +5,45 @@ pc_wins = 0
 
 variant = ["rock", "paper", "scissors"]
 
+mapping = {
+    "rock": "Rock",
+    "paper": "Paper",
+    "scissors": "Scissors"
+}
+
 while True:
-     input_actions = input("Rock/Paper/Scissors (R / P / S) or Quit (Q): ").lower()
-     if input_actions == "q":
-          break
-     
-     if input_actions not in ["r", "p", "s"]:
-          continue
-     
-     random_actions = random.randint(0,2)
-     
-     pc_pick = variant[random_actions]
-     
-     mapping = {
-    "r": "Rock",
-    "p": "Paper",
-    "s": "Scissors"
-    }
-     
-     print("Yuno picked ", mapping[pc_pick])
-     
-     if input_actions == "p" and pc_pick == "r":
-          print("You Won haha!")
-          player_wins += 1
-          
-     elif input_actions == "r" and pc_pick == "s":
-          print("You Won baka!")
-          player_wins += 1
-          
-     elif input_actions == "s" and pc_pick == "p":
-          print("You Won OwO!")
-          player_wins += 1
-          
-     else:
-          print("You think u can beat me! Baka, You Lost")
-          pc_wins +=1
+    input_actions = input("Rock/Paper/Scissors (R / P / S) or Quit (Q): ").lower()
+    if input_actions == "q":
+        break
+    
+    if input_actions not in ["r", "p", "s"]:
+        continue
+    
+    random_actions = random.randint(0, 2)
+    pc_pick = variant[random_actions]
+
+    print("Yuno picked", mapping[pc_pick])
+
+    if input_actions == "p" and pc_pick == "rock":
+        print("You Won haha!")
+        player_wins += 1
+        
+    elif input_actions == "r" and pc_pick == "scissors":
+        print("You Won baka!")
+        player_wins += 1
+        
+    elif input_actions == "s" and pc_pick == "paper":
+        print("You Won OwO!")
+        player_wins += 1
+        
+    elif (input_actions == "r" and pc_pick == "rock") or \
+         (input_actions == "p" and pc_pick == "paper") or \
+         (input_actions == "s" and pc_pick == "scissors"):
+        print("Draw!")  
+    else:
+        print("You think u can beat me! Baka, You Lost")
+        pc_wins += 1
+
 print(f"You Won {player_wins} times.")
 print(f"Yuno won {pc_wins} times.")
 print("Sayonara!!!!")
-          
