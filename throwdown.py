56a0cc7d10dc79528cfa6a2bd0c352bedc566ce9
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2025-08-27 21:48:48 +0600

    User Input quit actions

--- throwdown.py
+++ throwdown.py
@@ -1,1 +1,10 @@
-print('Connecting...')
+import random
+
+player_wins = 0
+pc_wins = 0
+
+while True:
+     input_actions = input("Rock/Paper/Scissors or Quit").lower()
+     if input_actions == "quit":
+          quit()
+     
